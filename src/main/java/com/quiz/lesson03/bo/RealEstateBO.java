0745e52201d6b6c32fa85cf4b58498d99a5c7f59
TestDesktop <test@test.com>
2023-12-19 18:43:11 +0900

    12/19 학원

--- src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -15,13 +15,13 @@ public class RealEstateBO {
 	@Autowired
 	private RealEstateMapper realEstateMapper;
 
-	// 1.
+	// 1-1.
 	// input: id / output: RealEstate(있거나 혹은 NULL)
 	public RealEstate getRealEstateById(int id) {
 		return realEstateMapper.selectRealEstateById(id);
 	}
 	
-	// 2.
+	// 1-2.
 	// input : rentPrice / output : List<>([] or 채워져있거나)
 	public List<RealEstate> getRealEstateByRentPrice(int rentPrice) {
 		// 여기서 input은 Controller에서 무조건 들어오게끔 되어 있으므로 int 타입
@@ -29,15 +29,26 @@ public class RealEstateBO {
 		return realEstateMapper.selectRealEstateByRentPrice(rentPrice);
 	}
 	
-	// 3.
+	// 1-3.
 	// input: area, price / output: List<>([] or 채워져있거나)
-	public List<RealEstate> getRealEstateByAreaPrice(
+	public List<RealEstate> getRealEstateByAreaPrice(Map<String, Object> areaPrice) {
 //			int area, int price
-			Map<String, Object> areaPrice
-			) {
-		return realEstateMapper.selectRealEstateByAreaPrice(
+		return realEstateMapper.selectRealEstateByAreaPrice(areaPrice);
 //				area, price
-				areaPrice
-				);
+	}
+	
+	
+	// 2-1.
+	// input: RealEstate / output: int(성공 행 수)
+	public int addRealEstate(RealEstate realEstate) {
+		return realEstateMapper.insertRealEstate(realEstate);
+	}
+	
+	// 2-2
+	// input: 6가지 params / output: int
+	public int addRealEstateAsField(
+			int realtorId, String address, int area, 
+			String type, int price, Integer rentPrice) {
+		return realEstateMapper.insertRealEstateAsField(realtorId, address, area, type, price, rentPrice);
 	}
 }
--- src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
+++ src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
@@ -3,6 +3,7 @@ package com.quiz.lesson03.mapper;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.quiz.lesson03.domain.RealEstate;
@@ -10,17 +11,35 @@ import com.quiz.lesson03.domain.RealEstate;
 @Repository
 public interface RealEstateMapper {
 	
-	// 1.
+	// 1-1.
 	public RealEstate selectRealEstateById(int id);
 	
-	// 2.
+	// 1-2.
 	// input : rentPrice / output : List<>([] or 채워져있거나)
 	public List<RealEstate> selectRealEstateByRentPrice(int rentPrice);
 	
-	// 3.
+	// 1-3.
 	// input: area, price / output: List<>([] or 채워져있거나)
 	public List<RealEstate> selectRealEstateByAreaPrice(
 //			@Param("area") int area,
 //			@Param("price") int price);
+			// 이게 최근 방식이긴 함
+			// xml로 단 한개의 파라미터만 보낼 수 있다. -> 이 때, @Param을 사용. 여러 개의 파라미터를 map에 담아 보낸다.
+			
 			Map<String, Object> areaPrice);
+	
+	
+	// 2-1.
+	// input: RealEstate / output: int(성공 행 수)
+	public int insertRealEstate(RealEstate realEstate); 
+		
+	
+	// 2-2.
+	public int insertRealEstateAsField(
+		@Param("realtorId") int realtorId, 
+		@Param("address") String address, 
+		@Param("area") int area, 
+		@Param("type") String type, 
+		@Param("price") int price, 
+		@Param("rentPrice") Integer rentPrice);
 }
